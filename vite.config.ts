c8e013d241c2e74b7e5fa0fed32d586e0cc0df90
Bethany Fannin <contact@bethanyfannin.com>
2025-03-10 15:13:06 -0400

    tests): update the testing environment setup to use jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
   test: {
     globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setupTests.ts'],
   },
 })
